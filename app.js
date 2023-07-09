0a05db0f9b65c556f61285a81ef5e43c80c7e943
shivani.rastogi <shivani.rastogi@99acres.com>
2023-07-09 23:40:19 +0530

    jsx | react element

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement( //React.createElement is an object
-    'div',
-    {id: "parent"}, //attributes
-    [
-        React.createElement('div',{id: "child1"},"hello from child 1"),
-        React.createElement('div',{id: "child2"},"hello from child 2")
-    ] //children    
-);
+// const heading = React.createElement( //React.createElement is an object
+//     'div',
+//     {id: "parent"}, //attributes
+//     [
+//         React.createElement('div',{id: "child1"},"hello from child 1"),
+//         React.createElement('div',{id: "child2"},"hello from child 2")
+//     ] //children    
+// );
+const para = <p>heading is :</p> //react element
+
+const Content = () =>{      //functional component
+    return <h1>hello world</h1> 
+}
+const Heading = () =>{  
+    return <div>
+        {para}
+        <Content />     
+    </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<Heading />);
